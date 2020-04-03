40ef56951c07c63e30fbf7421eeec47e97bba13e
lidiya edilgiriyeva <l.edilgiriyeva@portavita.ru>
2020-04-03 17:54:57 +0300

    remove logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,10 @@ import {persistStore} from "redux-persist";
 
 import rootReducer from "./rootReducer.js";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") middlewares.push(logger);
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
-
